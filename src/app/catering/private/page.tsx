dd5fd70134492242a7325e08c276dbbf8d7480dd
Faizan <faizan@codeus.in>
2026-03-06 22:31:39 +0530

    added sub routes

---
+++ src/app/catering/private/page.tsx
@@ -0,0 +1,9 @@
+import CateringSection from "@/components/sections/CateringSection";
+
+export default function PrivatePage() {
+  return (
+    <div className="pt-4">
+      <CateringSection />
+    </div>
+  );
+}
